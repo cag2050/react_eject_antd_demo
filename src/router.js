e5f7a18a4c39704892df367f8eb448a081230b5a
cag2050 <cag2050@163.com>
2018-04-08 14:58:19 +0800

    m

--- src/router.js
+++ src/router.js
@@ -5,13 +5,11 @@ export default [
   {
     path: '/',
     exact: true,
-    // component: ButtonDemo
     component(props) {
       return <Bundle {...props} load={() => import('./components/ButtonDemo')} />;
     }
   }, {
     path: '/input',
-    // component: InputDemo
     component(props) {
       return <Bundle {...props} load={() => import('./components/InputDemo')} />;
     }
