221807a52df4a21f37149eb0e9e2fb3aaa31f16a
cag2050 <cag2050@163.com>
2018-04-08 12:51:04 +0800

    m

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router} from 'react-router';
+import history from './history';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={history}>
+    <App/>
+  </Router>, document.getElementById('root'));
 registerServiceWorker();
