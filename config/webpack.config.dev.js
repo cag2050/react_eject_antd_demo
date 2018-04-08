28cc6d8efafe2f54621a91cf6f7faa4ff78f5ac7
cag2050 <cag2050@163.com>
2018-04-08 14:36:01 +0800

    m

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -144,9 +144,6 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              // "plugins": [
-              //   ["import", { "libraryName": "antd", "libraryDirectory": "es", "style": "css" }] // `style: true` 会加载 less 文件
-              // ],
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -149,9 +149,6 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              // "plugins": [
-              //   ["import", { "libraryName": "antd", "libraryDirectory": "es", "style": "css" }] // `style: true` 会加载 less 文件
-              // ],
               compact: true,
             },
           },
