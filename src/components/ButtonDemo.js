f00625fbfc744e7fb35977f4b0e78a01e17e5968
cag2050 <cag2050@163.com>
2018-04-09 18:56:12 +0800

    m

--- src/components/ButtonDemo.js
+++ src/components/ButtonDemo.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import {Button} from 'antd';
+import history from '../history'
 
 class ButtonDemo extends React.Component {
+  linkTo = () => {
+    history.push('/input')
+  }
+
   render() {
     return (
       <div>
-        <Button>BUTTON</Button>
+        <Button onClick={this.linkTo}>BUTTON</Button>
       </div>
     )
   }
