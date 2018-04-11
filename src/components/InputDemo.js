55186ff42cb2ced59b7d6068c040a5e077745969
cag2050 <cag2050@163.com>
2018-04-11 11:29:18 +0800

    m

--- src/components/InputDemo.js
+++ src/components/InputDemo.js
@@ -2,6 +2,12 @@ import React from 'react'
 import {Input} from 'antd';
 
 class InputDemo extends React.Component {
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps.location)
+    console.log(nextProps)
+    console.log(this.props.location)
+  }
+
   render() {
     return (
       <div>
