bff0072f658ef482edb062753294cd1b58f9af82
cag2050 <cag2050@163.com>
2018-04-16 12:03:58 +0800

    m

--- src/index.js
+++ src/index.js
@@ -12,6 +12,31 @@ import registerServiceWorker from './projectTools/registerServiceWorker';
 import {Provider} from 'react-redux';
 import store from './store';
 import Login from "./components/Login";
+import axios from 'axios'
+
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  console.log('config =')
+  console.log(config)
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+  // Do something with response data
+  console.log('response =')
+  console.log(response)
+  return response;
+}, function (error) {
+  // Do something with response error
+  return Promise.reject(error);
+});
+
+axios.get('/aaa')
 
 ReactDOM.render(
   <Provider store={store}>
