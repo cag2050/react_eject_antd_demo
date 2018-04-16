96989af29e01269e42988c239eed842ed078ee04
cag2050 <cag2050@163.com>
2018-04-16 15:50:05 +0800

    m

--- src/projectTools/history.js
+++ src/projectTools/history.js
@@ -2,4 +2,12 @@ import createHistory from 'history/createBrowserHistory';
 
 const history = createHistory();
 
+// 每次刷新页面时，执行
+// console.log('每次刷新页面时，执行')
+
+export const unlisten = history.listen(() => {
+  // 每次前端路由跳转时，执行
+  // console.log('每次前端路由跳转时，执行')
+});
+
 export default history;
