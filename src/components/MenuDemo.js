ddfef2994c109073c049c6465bf1d8d793abe217
cag2050 <cag2050@163.com>
2018-04-16 14:47:21 +0800

    m

--- src/components/MenuDemo.js
+++ src/components/MenuDemo.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Menu, Icon } from 'antd';
 import { NavLink, withRouter } from 'react-router-dom'
 
+// 这是一个 无状态组件，使用的是 函数式组件 的写法。
+// React.js 非常鼓励无状态组件，在 0.14 版本引入了函数式组件——一种定义不能使用 state 组件
 const MenuDemo = withRouter(({history}) => {
   return (
     <div>
