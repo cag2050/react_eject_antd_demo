580fdbaebda0e91c5693d16b67873a21a2f8f7ec
cag2050 <cag2050@163.com>
2018-04-20 12:00:41 +0800

    m

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -150,6 +150,14 @@ module.exports = {
               cacheDirectory: true,
             },
           },
+          {
+            test: /\.stylus$/,
+            use: [
+              require.resolve('style-loader'),
+              require.resolve('css-loader'),
+              require.resolve('stylus-loader')
+            ]
+          },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
           // "style" loader turns CSS into JS modules that inject <style> tags.
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -152,6 +152,31 @@ module.exports = {
               compact: true,
             },
           },
+          {
+            test: /\.stylus$/,
+            loader: ExtractTextPlugin.extract(
+              Object.assign(
+                {
+                  fallback: require.resolve('style-loader'),
+                  use: [
+                    {
+                      loader: require.resolve('css-loader'),
+                      options: {
+                        importLoaders: 1,
+                        minimize: true,
+                        sourceMap: shouldUseSourceMap,
+                      },
+                    },
+                    {
+                      loader: require.resolve('stylus-loader'),
+                    }
+                  ],
+                },
+                extractTextPluginOptions
+              )
+            ),
+            // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+          },
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
